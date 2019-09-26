c8ef11dbfc8c69a8640b1b6ac76d4758e3494132
Skillhh <ortegon.giovanny@hotmail.com>
2019-09-26 16:38:56 -0500

    [ADD] adding file pall

--- pall.c
+++ pall.c
@@ -2,8 +2,9 @@
 /**
  * pall - entry list
  * @head: list to print
+ * @line_number: line number
  */
-void pall(stack_t **head, int line_number)
+void pall(stack_t **head, unsigned int line_number)
 {
 	(void) line_number;
 	stack_t *curr = (*head);
