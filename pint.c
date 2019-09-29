f4ba5ffaf2cbdefd0533955a4337557eaba843d9
sechchr22 <980@holbertonschool.com>
2019-09-29 23:07:42 +0000

    pint fixed

--- pint.c
+++ pint.c
@@ -7,8 +7,16 @@
  */
 void pint(stack_t **head, unsigned int line_number)
 {
+	free_t struct_t;
 	stack_t *curr = (*head);
 	(void) line_number;
+
+	if (curr == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_close(struct_t, *head);
+		exit(EXIT_FAILURE);
+	}
 
 	printf("%d\n", curr->n);
 }
